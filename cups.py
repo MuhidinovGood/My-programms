c2ae4a93c7ccd5ae887ec4d5ccddf88296d9ebc5
MuhidinovGood <goodgame8988@gmail.com>
2017-02-17 16:47:21 +0300

    Add files via upload

---
+++ cups.py
@@ -0,0 +1,120 @@
+while True:
+
+    print("play again")
+    while True:
+
+        print (""" first level
+             __________________   __________________   __________________
+             \                /   \                /   \                /
+              \              /     \              /     \              /
+               \            /  or   \            /  or   \            /
+                \          /         \          /         \          /
+                 \________/           \________/           \________/
+                   """)
+        x = input("1 first cup, 2 second cup, 3 third cup")
+        if x == "3":
+            print("wrong, game over!!!")
+            break
+        elif x == "2":
+            print("wrong, game over!!!")
+            break
+        else:
+            print("right, let's go")
+
+        print(""" second level
+             __________________   __________________   __________________
+             \                /   \                /   \                /
+              \              /     \              /     \              /
+               \            /  or   \            /  or   \            /
+                \          /         \          /         \          /
+                 \________/           \________/           \________/
+                       """)
+        x = input("1 first cup, 2 second cup, 3 third cup")
+        if x == "1":
+            print("wrong, game over!!!")
+            break
+        elif x == "2":
+            print("wrong, game over!!!")
+            break
+        else:
+            print("right, let's go")
+
+        print(""" third level
+             __________________   __________________   __________________
+             \                /   \                /   \                /
+              \              /     \              /     \              /
+               \            /  or   \            /  or   \            /
+                \          /         \          /         \          /
+                 \________/           \________/           \________/
+                       """)
+        x = input("1 first cup, 2 second cup, 3 third cup")
+        if x == "3":
+            print("wrong, game over!!!")
+            break
+        elif x == "2":
+            print("wrong, game over!!!")
+            break
+        else:
+            print("right, let's go")
+
+        print(""" fourth level
+             __________________   __________________   __________________
+             \                /   \                /   \                /
+              \              /     \              /     \              /
+               \            /  or   \            /  or   \            /
+                \          /         \          /         \          /
+                 \________/           \________/           \________/
+                       """)
+        x = input("1 first cup, 2 second cup, 3 third cup")
+        if x == "3":
+            print("wrong, game over!!!")
+            break
+        elif x == "1":
+            print("wrong, game over!!!")
+            break
+        else:
+            print("right, let's go")
+
+        print(""" fifth level
+             __________________   __________________   __________________
+             \                /   \                /   \                /
+              \              /     \              /     \              /
+               \            /  or   \            /  or   \            /
+                \          /         \          /         \          /
+                 \________/           \________/           \________/
+                       """)
+        x = input("1 first cup, 2 second cup, 3 third cup")
+        if x == "2":
+            print("wrong, game over!!!")
+            break
+        elif x == "1":
+            print("wrong, game over!!!")
+            break
+        else:
+            print("right, let's go")
+
+        print(""" sixth level
+             __________________   __________________   __________________
+             \                /   \                /   \                /
+              \              /     \              /     \              / 
+               \            /  or   \            /  or   \            /
+                \          /         \          /         \          /
+                 \________/           \________/           \________/
+                       """)
+        x = input("1 first cup, 2 second cup, 3 third cup")
+        if x == "1":
+            print("wrong, game over!!!")
+            break
+        elif x == "2":
+            print("wrong, game over!!!")
+            break
+        else:
+            print("right, you passed the game")
+            break
+
+
+
+
+
+
+
